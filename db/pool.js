3fc5bdd282e50881abd9708294b5b2b1e5b90384
Avadhut <avadhutkaskar@gmail.com>
2026-01-14 22:13:34 +0530

    variable name changed

--- db/pool.js
+++ db/pool.js
@@ -2,7 +2,7 @@
 const {Pool} = require('pg');
 
 module.exports = new Pool({
-    connectionString: process.env.DB_URL,
+    connectionString: process.env.DATABASE_URL,
     ssl: process.env.NODE_ENV === "production"
     ? { rejectUnauthorized: false }
     : false,
